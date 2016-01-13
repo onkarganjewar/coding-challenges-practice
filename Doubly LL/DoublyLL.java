d48075bd2598cbb3eaaf7d868584c53684c00137
Onkar Ganjewar <ganjewaronkar@gmail.com>
2016-01-12 16:54:04 -0800

    added reverse using recursion

--- Doubly LL/DoublyLL.java
+++ Doubly LL/DoublyLL.java
@@ -3,7 +3,7 @@ import javax.swing.plaf.synth.SynthSeparatorUI;
 public class DoublyLL {
 	Node head = null ;
 	Node last;
-	public void insert(int data, Node n) 
+	public void insert(int data, Node n)
 	{
 		Node temp = new Node();
 		Node current = this.head;
@@ -27,13 +27,13 @@ public class DoublyLL {
 			current.next = temp;
 			temp.prev = current;
 		}
-		
+
 	}
-	public void display() 
+	public void display()
 	{
 		Node current = this.head;
 		int i = 1;
-		if (current == null) { 
+		if (current == null) {
 			System.out.println("Empty Linked list");
 			return;
 		}
@@ -42,20 +42,20 @@ public class DoublyLL {
 			System.out.println("[ "+current.data+" ]");
 			return;
 		}
-		else 
+		else
 		{
 			System.out.print("[");
 			while (current.next!= null)
 			{
-				System.out.print(" "+current.data+",");			
+				System.out.print(" "+current.data+",");
 				current = current.next;
 				i++;
 			}
-			System.out.print(" "+current.data+" ]");			
+			System.out.print(" "+current.data+" ]");
 		}
 	}
-	
-	public void insert_begin(int data, Node n) 
+
+	public void insert_begin(int data, Node n)
 	{
 		Node current = this.head;
 		Node temp = n;
@@ -67,7 +67,7 @@ public class DoublyLL {
 			current = temp;
 			this.head = current;
 		}
-		else 
+		else
 		{
 			current.prev = temp;
 			temp.next = current;
@@ -75,7 +75,7 @@ public class DoublyLL {
 			this.head = temp;
 		}
 	}
-	
+
 	//Inserting an element in the linked list at the position k with data d
 	public void insert_nth(int k, int d)
 	{
@@ -84,7 +84,7 @@ public class DoublyLL {
 		Node newnode = new Node();
 		newnode.data = d;
 		newnode.next = null;
-		
+
 		if (current.next == null)
 		{
 			current.next = newnode;
@@ -92,7 +92,7 @@ public class DoublyLL {
 			newnode.next = null;
 			return;
 		}
-		
+
 		for (int i=1; i<k ; i++)
 		{
 			current = current.next;
@@ -104,26 +104,26 @@ public class DoublyLL {
 		newnode.prev = prev;
 	}
 
-	
+
 	//Reversing the linked list using iterative method
-	public void reverse_it() 
+	public void reverse_it()
 	{
 		Node current = this.head;
-		Node tail = this.head; //saving the head pointer 
+		Node tail = this.head; //saving the head pointer
 		Node temp = new Node();
 		temp.data = 0;
-	
+
 		if(current == null)
 		{
 			System.out.println("Empty Linked List");
 			return;
 		}
-		
+
 		while (tail.next!= null)
 		{
-			tail = tail.next; //setting the tail at the end of the linked list 
+			tail = tail.next; //setting the tail at the end of the linked list
 		}
-		
+
 		while(current != null)
 		{
 			temp = current.next;
@@ -133,12 +133,43 @@ public class DoublyLL {
 		}
 		this.head = tail;
 	}
-		
-	public void delete_nth (int k) 
+
+
+	//Reversing the linked list using iterative method
+	public void reverse_rec(Node n)
+	{
+		Node current = n;
+		Node head = this.head;
+		Node tail = this.head; //saving the head pointer
+		Node temp;
+
+		if(current == null)
+		{
+			while (tail.next!= null)
+			{
+				tail = tail.next; //setting the tail at the end of the linked list
+			}
+			this.head = tail;
+			return;
+		}
+
+			reverse_rec(current.next);
+
+			temp = current.next;
+			current.next = current.prev;
+			current.prev = temp;
+	}
+
+
+
+
+
+
+	public void delete_nth (int k)
 	{
 		Node prev,next;
 		Node current = this.head;
-		
+
 		if(current == null)
 		{
 			System.out.println("Empty Linked List");
@@ -150,12 +181,12 @@ public class DoublyLL {
 			this.head = null;
 			return;
 		}
-		
+
 		for (int i = 1; i<k ; i++)
 		{
 			current = current.next;
 		}
-		
+
 		if(current.next == null)
 		{
 			prev = current.prev;
@@ -169,7 +200,7 @@ public class DoublyLL {
 			next.prev = prev;
 		}
 	}
-	
+
 	public void delete_head() {
 		Node current = this.head;
 		Node next;
@@ -189,9 +220,9 @@ public class DoublyLL {
 		next.prev = null;
 		this.head = next;
 	}
-	
+
 	public void display_reverse() {
-		
+
 		Node current = this.head;
 		if (current == null)
 		{
@@ -217,10 +248,10 @@ public class DoublyLL {
 		}
 		System.out.print(" "+current.data+" ]");
 	}
-	
+
 	public static void main(String[] args) {
 		DoublyLL dl = new DoublyLL();
-		
+		Node t = dl.head;
 		Node a = new Node();
 		Node b = new Node();
 		Node c = new Node();
@@ -229,31 +260,51 @@ public class DoublyLL {
 	//	dl.delete_nth(1);
 		dl.insert(30, d);
 		dl.display();
-		
+
 		dl.insert_begin(10, c);
 		System.out.println("\nLinked List after inserting 10 at the beginning ");
 		dl.display();
 
 		System.out.println("\nPrinting the linked list in the reverse order");
 		dl.display_reverse();
-		
+
 		dl.insert_nth(2, 22);
 		System.out.println("\nLinked List after inserting element 22 at the 2nd position");
 		dl.display();
-		
-		
+
+
 		dl.delete_nth(2);
 		System.out.println("\nLinked List after deletion of second element");
 		dl.display();
-	
+/*
 		dl.delete_head();
 		System.out.println("\nLinked list after removal of the head");
 		dl.display();
-		
-		
-		System.out.println("\nReversing of the linked list");		
+
+
+		System.out.println("\nReversing of the linked list");
 		dl.reverse_it();
+		dl.display();
+*/
+		System.out.println("\nReversing of the linked list using recursion");
+		dl.reverse_rec(dl.head);
 		dl.display();
 	}
 }
 
+
+/*Output :
+
+[ 20, 30 ]
+Linked List after inserting 10 at the beginning
+[ 10, 20, 30 ]
+Printing the linked list in the reverse order
+[ 30, 20, 10 ]
+Linked List after inserting element 22 at the 2nd position
+[ 10, 22, 20, 30 ]
+Linked List after deletion of second element
+[ 10, 20, 30 ]
+Reversing of the linked list using recursion
+[ 30, 20, 10 ]
+
+*/
