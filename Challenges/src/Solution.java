cd109c6c2a9900e87eb87e7e128315d74f0b4c68
Onkar Ganjewar <ganjewaronkar@gmail.com>
2017-06-08 21:09:19 -0700

    Add challenge -- Stack Max Element

--- Challenges/src/Solution.java
+++ Challenges/src/Solution.java
@@ -1027,4 +1027,44 @@ public class Solution {
 		} else
 			return false;
 	}
+	
+	/**
+	 * Get the maximum element from the stack. <br> Hackerrank Problem: <br> 3 choices --
+	 * <br> 1. Push element
+	 * <br> 2. Pop element
+	 * <br> 3. Print max element of stack at that point 
+	 * 
+	 */
+	public static void stackMaxElement() {
+		Scanner sc = new Scanner(System.in);
+	    int n = sc.nextInt();
+	    Stack<Integer> stack = new Stack<Integer>();
+	    Stack<Integer> maxStack = new Stack<Integer>();
+	        
+	    while (n > 0) {
+	        int choice = sc.nextInt();
+	        if (choice == 1) {
+	            Integer val = sc.nextInt();
+	            if (val >= getStackMax(maxStack))
+	            	maxStack.push(val);
+	            stack.push(val);
+	        } else if (choice == 2) {
+	             Integer value = stack.pop();
+	             if (value == getStackMax(maxStack)) 
+	                	maxStack.pop();
+	        } else if (choice == 3) {
+	            if (!maxStack.isEmpty()) {
+	                System.out.println(getStackMax(maxStack));
+	            }
+	        }
+	        n--;
+	    }
+	    sc.close();
+    }
+	
+	private static Integer getStackMax(Stack<Integer> s) {
+		if (s.isEmpty()) 
+			return Integer.MIN_VALUE;
+		return s.peek();
+	}
 }
