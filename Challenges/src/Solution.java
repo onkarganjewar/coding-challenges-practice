40c6bc7b5b1e58eca7bf12b30c2311b2ab7d6f7d
Onkar Ganjewar <ganjewaronkar@gmail.com>
2017-07-19 15:45:12 -0700

    Refactor code

--- Challenges/src/Solution.java
+++ Challenges/src/Solution.java
@@ -2441,11 +2441,24 @@ public class Solution {
 		Arrays.sort(a);
 
 		for (int i = 0; i < queries.length; i++) {
-			int id2 = binarySearchFirst(a, 0, a.length - 1, queries[i]);
+			int id2 = binarySearchUtil(a, 0, a.length - 1, queries[i]);
 			list.add(id2);
 		}
 
 		return list;
+	}
+
+	private static int binarySearchUtil(int arr[], int low, int high, int x) {
+		if (high >= low) {
+			int mid = low + (high - low) / 2;
+			if ((mid == 0 || x > arr[mid - 1]) && arr[mid] == x)
+				return mid;
+			else if (x > arr[mid])
+				return binarySearchUtil(arr, (mid + 1), high, x);
+			else
+				return binarySearchUtil(arr, low, (mid - 1), x);
+		}
+		return low;
 	}
 
 	/**
