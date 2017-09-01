408e4dfe10d9e262d7b00bf05ab9a3619491a645
Onkar Ganjewar <ganjewaronkar@gmail.com>
2017-09-01 12:07:23 -0700

    Add Challenge - Container With Most Water

--- Challenges/src/Solution.java
+++ Challenges/src/Solution.java
@@ -5168,4 +5168,39 @@ public class Solution {
 		return (j == m); // if the complete pattern(String s) is matched then
 							// return true
 	}
+
+	/**
+	 * Given n non-negative integers a1, a2, ..., an, where each represents a
+	 * point at coordinate (i, ai). n vertical lines are drawn such that the two
+	 * endpoints of line i is at (i, ai) and (i, 0). Find two lines, which
+	 * together with x-axis forms a container, such that the container contains
+	 * the most water. <br>
+	 * <br>
+	 * <b>Input</b>: {@code [1,3,2]} <br>
+	 * <b>Output</b>: 2<br>
+	 * <br>
+	 * <b>Input</b>:{@code [1, 8, 6, 2, 5, 4, 8, 3, 7]}<br>
+	 * <b>Output</b>:{@code 49}
+	 * 
+	 * @param heights
+	 *            a vector of integers
+	 * @return an integer
+	 */
+	public int maxArea(int[] heights) {
+		if (heights == null || heights.length < 2)
+			return 0;
+
+		int maxArea = 0, l = 0, r = heights.length - 1;
+		while (l < r) {
+			// area formed by rectangle will be
+			// area = base * min(height[i], heights[j])
+			maxArea = Math.max(Math.min(heights[l], heights[r]) * (r - l), maxArea);
+			// forward the left pointer if left wall is < right wall
+			if (heights[l] < heights[r])
+				l++;
+			else
+				r--;
+		}
+		return maxArea;
+	}
 }
--- README.md
+++ README.md
@@ -130,4 +130,5 @@ Programs implemented in Java that contains solutions to various coding challenge
 | 125 | [Happy Number](../master/Challenges/src/Solution.java#L5031) | [LeetCode](https://leetcode.com/problems/happy-number)
 | 126 | [Word Ladder](../master/Challenges/src/Solution.java#L5089) | [LintCode](http://www.lintcode.com/en/problem/word-ladder)
 | 127 | [Is Subsequence](../master/Challenges/src/Solution.java#L5155) | [LeetCode](https://leetcode.com/problems/is-subsequence/)
+| 128 | [Container With Most Water](../master/Challenges/src/Solution.java#L5189) | [LeetCode](https://leetcode.com/problems/container-with-most-water)
 
